e849813906a79937c5e555996169e8789c8e0d9b
Balint Joo <bjoo@jlab.org>
2014-11-03 10:54:47 -0800

    Enabled linking against multigrid here

--- parscalar/build_chroma.sh
+++ parscalar/build_chroma.sh
@@ -27,16 +27,15 @@ ${SRCDIR}/chroma/configure --prefix=${INSTALLDIR}/chroma \
         CC="${PK_CC}"  CXX="${PK_CXX}" \
 	CXXFLAGS="${PK_CXXFLAGS}" CFLAGS="${PK_CFLAGS}" \
         --enable-sse-scalarsite-bicgstab-kernels --host=x86_64-linux-gnu --build=none \
-	--with-qmp=${INSTALLDIR}/qmp
-# \
-#	--with-mdwf=${INSTALLDIR}/mdwf \
-#        --with-qla=${INSTALLDIR}/qla \
-#        --with-qio=${INSTALLDIR}/qio \
-#        --with-qdpc=${INSTALLDIR}/qdpc \
-#        --with-qopqdp=${INSTALLDIR}/qopqdp \
-#        --enable-qop-mg
+	--with-qmp=${INSTALLDIR}/qmp \
+	--with-mdwf=${INSTALLDIR}/mdwf \
+        --with-qla=${INSTALLDIR}/qla \
+        --with-qio=${INSTALLDIR}/qio \
+        --with-qdpc=${INSTALLDIR}/qdpc \
+        --with-qopqdp=${INSTALLDIR}/qopqdp \
+        --enable-qop-mg
 
 ${MAKE}
-#${MAKE} install
+${MAKE} install
 
 popd
