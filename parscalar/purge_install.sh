e6015472b98e968d73508571f875fa854189e5df
Balint Joo <bjoo@jlab.org>
2014-11-03 12:16:37 -0800

    Make it remove the chroma sp install too

--- parscalar/purge_install.sh
+++ parscalar/purge_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./env.sh
 pushd $INSTALLDIR
-rm -rf chroma-double  libxml2  mdwf  qdp++  qdp++-double  qdpc  qio  qla  qmp  qopqdp
+rm -rf chroma-double chroma libxml2  mdwf  qdp++  qdp++-double  qdpc  qio  qla  qmp  qopqdp
 popd
