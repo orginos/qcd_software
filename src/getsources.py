fe0de03162da51d941c719d45a8e22703e5447b2
bjoo <bjoo@jlab.org>
2014-11-13 10:11:33 -0500

    Changed so that tagged checkouts actually work, hopefully

--- src/getsources.py
+++ src/getsources.py
@@ -7,10 +7,25 @@ from subprocess import call
 import os
 
 
-def gitGet(pkg_name, git_url, branch):
-    command = ("git clone --recursive "+git_url+" -b "+branch).split()
-    call(command)
-    
+def gitGet(pkg_name, git_url, branch,tag=False):
+    if tag == False:
+        # its a real branch you can check it out
+        command = ("git clone --recursive "+git_url+" -b "+branch).split()
+        call(command)
+    else:
+        # its a tag, not a real branch:
+        command = ("git clone --recursive "+git_url).split()
+        call(command)
+	cwd=os.getcwd();
+	print "Changing dir to "+cwd+"/"+pkg_name
+	os.chdir(cwd+"/"+pkg_name)
+	print "Checking out tag "+branch
+        command = ("git checkout "+branch).split()
+        call(command)
+	print "Changing back to "+cwd
+ 	os.chdir(cwd)
+
+
     
 def wgetGet(pkg_name, wget_url, releasename):
     zipname=releasename+".tar.gz"
@@ -36,34 +51,33 @@ def tarGet(pkg_name, tar_file, untar_name):
     call(command)
  
 # QDP++ and Chroma
-chroma = ("chroma", "GIT", "git@github.com:JeffersonLab/chroma.git","master")
-qdpxx = ("qdpxx", "GIT", "git@github.com:usqcd-software/qdpxx.git", "master")
-qdp_jit=("qdp-jit", "GIT", "git@github.com:fwinter/qdp-jit","master")
-qmp=("qmp", "GIT", "git@github.com:usqcd-software/qmp.git", "master")
-quda=("quda", "GIT", "git@github.com:lattice/quda.git", "quda-0.7")
-qphix=("qphix", "GIT", "git@github.com:JeffersonLab/qphix.git", "master")
+chroma = ("chroma", "GIT", "git@github.com:JeffersonLab/chroma.git","master",False)
+qdpxx = ("qdpxx", "GIT", "git@github.com:usqcd-software/qdpxx.git", "master",False)
+qdp_jit=("qdp-jit", "GIT", "git@github.com:fwinter/qdp-jit","master",False)
+qmp=("qmp", "GIT", "git@github.com:usqcd-software/qmp.git", "master",False)
+quda=("quda", "GIT", "git@github.com:lattice/quda.git", "quda-0.7",False)
+qphix=("qphix", "GIT", "git@github.com:JeffersonLab/qphix.git", "master",False)
 
 # QDP/C libraries for Multigrid
-qdpc=("qdp", "GIT", "git@github.com:usqcd-software/qdp.git", "qdp1-10-0")
-qio=("qio", "GIT", "git@github.com:usqcd-software/qio.git", "qio2-3-9")
-qla=("qla", "GIT", "git@github.com:usqcd-software/qla.git", "qla1-8-0")
-qopqdp=("qopqdp", "GIT", "git@github.com:usqcd-software/qopqdp.git", "qopqdp0-19-3")
+qdpc=("qdp", "GIT", "git@github.com:usqcd-software/qdp.git", "qdp1-10-0",True)
+qio=("qio", "GIT", "git@github.com:usqcd-software/qio.git", "qio2-3-9",True)
+qla=("qla", "GIT", "git@github.com:usqcd-software/qla.git", "qla1-8-0",True)
+qopqdp=("qopqdp", "GIT", "git@github.com:usqcd-software/qopqdp.git", "qopqdp0-19-3", True)
 
 # These two pacakges are annoyying to get via GITHUB, so I have to package them  :(
 
-libxml2=("libxml2", "TARGZ", "./libxml2.tar.gz", "libxml2")
-mdwf=("mdwf", "TARGZ", "./mdwf-1.1.4.tar.gz", "mdwf-1.1.4")
+libxml2=("libxml2", "TARGZ", "./libxml2.tar.gz", "libxml2",True)
+mdwf=("mdwf", "TARGZ", "./mdwf-1.1.4.tar.gz", "mdwf-1.1.4",True)
 
 # FULL PACKAGE LIST -- I distribute this because I have the 'ansidecl' thing commented out
 package_list=[ chroma, qdpxx, qdp_jit, qmp, mdwf, quda, qphix, qdpc, qio, qla, qopqdp, libxml2 ]
 
-
 for pkg in package_list:
-        (name,method,url,branchname)=pkg 
+        (name,method,url,branchname,tagP)=pkg 
                  
         print "Getting Package "+name+" via "+method+" from "+url
         if method =="GIT" :
-            gitGet(name,url,branchname)
+            gitGet(name,url,branchname,tagP)
         elif method == "WGET":   
             wgetGet(name,url,branchname)
         elif method == "CURL":
