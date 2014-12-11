8e706a07cbb2b4c89464fdd9f8a5a1687847526e
Balint Joo <bjoo@jlab.org>
2014-12-10 23:20:24 -0500

    Added builder script for scalar QDP++ and ascalar Dslash(SOA=4)

---
+++ avx/build_qdp++-scalar.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+#################
+# BUILD QMP
+#################
+source env.sh
+
+pushd ${SRCDIR}/qdpxx
+aclocal; automake; autoconf
+popd
+
+pushd ${BUILDDIR}
+
+if [ -d ./build_qdpxx-scalar-avx ]; 
+then 
+  rm -rf ./build_qdpxx-scalar-avx
+fi
+
+mkdir  ./build_qdpxx-parscalar-avx
+cd ./build_qdpxx-parscalar-avx
+
+
+${SRCDIR}/qdpxx/configure \
+	--prefix=${INSTALLDIR}/qdp++-scalar \
+        --enable-parallel-arch=scalar \
+        --disable-filedb \
+        --enable-largefile \
+        --enable-parallel-io \
+        --enable-alignment=64 \
+	--with-libxml2=${INSTALLDIR}/libxml2\
+	CXXFLAGS="${PK_CXXFLAGS}" \
+	CFLAGS="${PK_CFLAGS}" \
+	CXX="${PK_CXX}" \
+	CC="${PK_CC}" \
+	--host=x86_64-linux-gnu --build=none \
+	${OMPENABLE}
+
+${MAKE}
+${MAKE} install
+
+popd
