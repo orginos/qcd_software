8999a36360057020f809a50377862d3dc6f967bd
Kostas Orginos <kostas@wm.edu>
2015-06-01 16:57:15 -0400

    added MacOSX built for qcdc qopdp qio

--- MacOSX/build_qdpc.sh
+++ MacOSX/build_qdpc.sh
@@ -4,28 +4,26 @@
 # BUILD QMP
 #################
 source env.sh
-
-pushd ${SRCDIR}/qio
-autoreconf 
-#automake --add-missing
+pushd ${SRCDIR}/qdp
+autoreconf
 popd
 
 pushd ${BUILDDIR}
 
-if [ -d ./build_qio ]; 
+if [ -d ./build_qdp ]; 
 then 
-  rm -rf ./build_qio
+  rm -rf ./build_qdp
 fi
 
-mkdir  ./build_qio
-cd ./build_qio
+mkdir  ./build_qdp
+cd ./build_qdp
 
 
-${SRCDIR}/qio/configure \
-	--prefix=${INSTALLDIR}/qio \
-	--enable-largefile \
-	--enable-dml-output-buffering \
+${SRCDIR}/qdp/configure \
+	--prefix=${INSTALLDIR}/qdpc \
 	--with-qmp=${INSTALLDIR}/qmp \
+	--with-qio=${INSTALLDIR}/qio \
+	--with-qla=${INSTALLDIR}/qla \
 	CFLAGS="${PK_CFLAGS}" \
 	CC="${PK_CC}" \
 	--host=x86_64-linux-gnu --build=none \
