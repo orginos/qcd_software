c5250cacc45e9a1643c192a67e4c8862ea250a23
Kostas Orginos <kostas@wm.edu>
2015-06-02 08:29:11 -0700

    removed edison

--- edison/purge_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ./env.sh
-pushd $BUILDDIR
-rm -rf build_chroma  build_chroma-double  build_libxml2  build_mdwf  build_qdp++  build_qdp++-double  build_qdpc  build_qio  build_qla  build_qmp  build_qopqdp
-popd
