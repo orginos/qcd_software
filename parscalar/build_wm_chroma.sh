624be9f72c4e5f9afe9a662c03e3c98fe444caab
Kostas Orginos <kostas@wm.edu>
2015-11-25 14:10:12 -0500

    added new parscalar built for wm_chroma

--- parscalar/build_wm_chroma.sh
+++ parscalar/build_wm_chroma.sh
@@ -8,30 +8,19 @@ source env.sh
 
 pushd ${BUILDDIR}
 
-if [ -d ./build_wm_chroma ]; 
+if [ -d ./build_wm_chroma-scalar ]; 
 then 
-  rm -rf ./build_wm_chroma
+  rm -rf ./build_wm_chroma-scalar
 fi
 
 mkdir  ./build_wm_chroma
 cd ./build_wm_chroma
-cp -p -r ${SRCDIR}/wm_chroma/* .
 
-cat > Makefile<<EOF
-CHROMA_DIR = ${INSTALLDIR}/chroma
-ARPREC_DIR = ${INSTALLDIR}/arprec
-QD_DIR = ${INSTALLDIR}/qd
-ITPP_DIR = ${INSTALLDIR}/itpp
-include Make.inc
-EOF
+CHROMA_DIR=/usr/local/scidac/chroma/scalar
+
+cmake -DCHROMA_PREFIX_DIR=${CHROMA_DIR} -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/wm_chroma/scalar ${SRCDIR}/wm_chroma
 
 ${MAKE}
-INSTALL_DIR=/usr/local/scidac/wm_chroma/scalar 
-if [ -d $INSTALL_DIR ]; 
-then 
-  rm -rf $INSTALL_DIR
-fi
-mkdir -p $INSTALL_DIR
-cp wm_chroma $INSTALL_DIR
+${MAKE} install
 
 popd
