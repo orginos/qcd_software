835358ba7fbd919ba4aa9e79ed2e00fcc8c2c4bd
Kostas Orginos <kostas@jlab.org>
2016-03-03 13:19:19 -0500

    added adat to jlab

---
+++ jlab/scalar/build_adat.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#################
+# BUILD Adat
+#################
+source env.sh
+
+pushd ${SRCDIR}/adat
+autoreconf -f -i 
+popd
+
+pushd ${BUILDDIR}
+
+if [ -d ./build_adat ]; 
+then 
+  rm -rf ./build_adat
+fi
+
+mkdir  ./build_adat
+cd ./build_adat
+
+GMP=/dist/gmp-6.0.0
+
+${SRCDIR}/adat/configure --prefix=${INSTALLDIR}/adat \
+    CC="${PK_CC}"  CXX="${PK_CXX}" \
+    CXXFLAGS=" ${PK_CXXFLAGS} -I${GMP}/include" \
+    LIBS="$GMP/lib/libgmpxx.a $GMP/lib/libgmp.a"
+
+#-I ${GMP}/include " \
+#     LIBS="$GMP/lib64/libgmpxx.a $GMP/lib64/libgmp.a"
+
+# LDFLAGS="-L ${GMP}/lib"  \
+#     LIBS="-lgmpxx -lgmp"
+
+${MAKE}
+${MAKE} install
+
+popd
