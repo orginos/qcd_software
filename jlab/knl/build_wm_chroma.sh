83ffd62e39fb1d07bb77acc867a7ae8d665d417a
Kostas Orginos <kostas@jlab.org>
2017-02-20 22:30:17 -0500

    now wm_chroma compiles with new setup for LAPACK. see INSTALL

--- jlab/knl/build_wm_chroma.sh
+++ jlab/knl/build_wm_chroma.sh
@@ -18,7 +18,7 @@ cd ./build_wm_chroma
 
 CHROMA_DIR=${INSTALLDIR}/chroma
 
-cmake -DCHROMA_PREFIX_PATH=${CHROMA_DIR} -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/wm_chroma -DCMAKE_CXX_COMPILER=${PK_CXX}  ${SRCDIR}/wm_chroma
+cmake -DCHROMA_PREFIX_PATH=${CHROMA_DIR} -DCMAKE_INSTALL_PREFIX=${INSTALLDIR}/wm_chroma -DCMAKE_CXX_COMPILER=${PK_CXX}  -DLAPACK_LD="-L${MKLROOT}/lib/intel64" -DLAPACK_LIBS="-Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_sequential.a ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group" ${SRCDIR}/wm_chroma
 
 ${MAKE}
 ${MAKE} install
