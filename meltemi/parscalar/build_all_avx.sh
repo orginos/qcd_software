a5d349472bb02edd403c1c4edcd1ac6636422c3e
Kostas Orginos <kostas@wm.edu>
2018-11-20 14:44:36 -0500

    one more thing...

--- meltemi/parscalar/build_all_avx.sh
+++ meltemi/parscalar/build_all_avx.sh
@@ -5,21 +5,20 @@
 
 # Build everything
 ./build_qmp.sh
-#./build_qdp++-tbb.sh
 ./build_qdp++.sh
 ./build_qdp++-double.sh
-#./build_qdp++-tbb_3d.sh
 ./build_qdp++_3d.sh
 ./build_qdp++-double_3d.sh
 ./build_qphix_cmake.sh
 ./build_qphix_cmake-double.sh*
-#./build_qdp++-double-tbb.sh
+./build_mgproto_cmake.sh
+./build_mgproto_cmake-double.sh
 
 ./build_chroma.sh
 ./build_chroma-double.sh
 ./build_wm_chroma.sh
+./build_wm_chroma-double.sh 
 
 ./build_harom.sh
-
 
 #./build_chroma-double.sh
